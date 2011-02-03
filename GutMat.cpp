251adc924dabe2f9132923564ea9c08b81f8e6b1
Brecht Verstichel <brecht.verstichel@ugent.be>
2011-02-03 17:58:48 +0100

    programmed the p and q sos maps
    they work

--- GutMat.cpp
+++ GutMat.cpp
@@ -62,4 +62,52 @@ ostream &operator<<(ostream &output,GutMat &gm_p){
 
 }
 
+/**
+ * makes the approximated spm on singly occupied space
+ * @param tpm The input TPM
+ */
+void GutMat::p(const TPM &tpm){
+
+   *this = 0.0;
+
+   SPM spm(1.0/(N - 1.0),tpm);
+
+   for(int a = 0;a < M/2;++a){
+
+      (*this)(a,a) = spm(2*a + 1,2*a + 1);
+
+      for(int b = a;b < M/2;++b)
+         (*this)(a,b) += spm(2*a,2*b) - tpm(2*a,2*b+1,2*b,2*b+1) - tpm(2*a,2*a+1,2*b,2*a+1);
+
+   }
+
+   this->symmetrize();
+
+}
+
+/**
+ * makes the approximated one hole matrix on singly occupied space
+ * @param tpm The input TPM
+ */
+void GutMat::q(const TPM &tpm){
+
+   *this = 0.0;
+
+   SPM spm(1.0/(N - 1.0),tpm);
+
+   double ward = 2.0*tpm.trace()/(N*(N - 1.0));
+
+   for(int a = 0;a < M/2;++a){
+
+      (*this)(a,a) = ward - spm(2*a + 1,2*a + 1);
+
+      for(int b = a;b < M/2;++b)
+         (*this)(a,b) += tpm(2*b + 1,2*b,2*b+1,2*a) + tpm(2*a+1,2*b,2*a+1,2*a) - spm(2*a,2*b);
+
+   }
+
+   this->symmetrize();
+
+}
+
 /* vim: set ts=3 sw=3 expandtab :*/
